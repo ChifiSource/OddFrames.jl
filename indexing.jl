8a64446a35fc1260ae394f5bdf5e1dacb9aadf08
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-01-26 09:19:34 -0500

    Delete indexing.jl

--- indexing.jl
+++
@@ -1,6 +0,0 @@
-import Base: getindex
-getindex(od::OddFrame, col::Symbol) = od.lookup[col]
-getindex(od::OddFrame, col::String) = od.lookup[Symbol(col)]
-function getindex(od::OddFrame, mask::BitArray)
-    [if mask[i] == 0 drop(od, i) end for i in 1:length(mask)]
-end
