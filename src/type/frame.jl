c7897ea315d237e8df871b5f5a8a08727badddc0
Em <52672675+emmettgb@users.noreply.github.com>
2021-08-29 12:33:49 -0400

    rewrote frame with indexing approach

--- src/type/frame.jl
+++ src/type/frame.jl
@@ -1,68 +1,94 @@
 include("css.jl")
+using Lathe.stats: mean
 import Base: getindex
 #=========
 o=o= Heirarchy overview =o=o
 |AbstractOddFrame
 |_____ OddFrame
+|_____ ImmutableOddFrame
 |_____ ImageOddFrame
 =========#
 abstract type AbstractOddFrame end
 #=============
 OddFrame Type
 =============#
-mutable struct OddFrame{head} <: AbstractOddFrame
-    lookup::Dict
-    head::head
-    function OddFrame(lookup::Dict)
-        """Docstring test"""
-        head(count; style = "classic") = _head(lookup, count, style = style)
-        new{typeof(head)}(lookup, head)
-    end
-function _head(lookup::Dict, count::Int64 = 5; style::String = "classic")
-    thead = "<thead>
-<tr>"
-    tbody = "<tbody>"
-    [thead = string(thead, "<th>", string(name), "</th>") for name in keys(lookup)]
-    thead = string(thead, "</thead>")
-    cols = values(lookup)
-    features = [push!(val) for val in cols]
-    for i in 1:length(features[1])
-        obs = [row[i] for row in features]
-        tbody = string(tbody, "<tr>")
-        [tbody = string(tbody, "<td>", observ, "</td>") for observ in obs]
-        tbody = string(tbody, "</tr>")
-    end
-    tbody = string(tbody, "</tbody>")
-    compisition = string(_css,"<table class = \"", style, "\">",
-     thead, tbody,"</table>")
-     try
-         display("text/html", compisition)
-     catch
-         md = ""
-         throw("Terminal display has not yet been implemented.")
-    end
+mutable struct OddFrame <: AbstractOddFrame
+        labels::Array{Symbol}
+        columns::Array{Any}
+        coldata::String
+        head::Function
+        drop::Function
+        #==
+        Constructors
+        ==#
+        function OddFrame(p::Pair ...)
+                labels = [p[1] for pair in p]
+                columns = [p[2] for pair in p]
+                length_check(columns)
+                coldata = generate_coldata(columns)
+                head(x::Int64) = _head(labels, columns, x)
+                head() = _head(labels, columns, 5)
+                new(lookup, head, drop)
+        end
+        function OddFrame(file_path::String)
+                extensions = Dict(".csv" => read_csv)
+                extension = split(file_path, '.')[2]
+                values, columns = extensions[extension](file_path)
+                coldata = generate_coldata(columns::Array)
+                
+        end
+        #==
+        Supporting
+                Functions
+        ( Support constructors )
+        ==#
+        function generate_coldata(columns::Array)
+
+        end
+        #==
+        Child
+            methods
+            ==#
+        function _head(labels::Array{Symbol},
+                columns::Array{Any}, count::Int64)
+                thead = "<thead><tr>"
+                tbody = "<tbody>"
+                [thead = string(thead, "<th>", string(name),
+                 "</th>") for name in keys(lookup)]
+                 thead = string(thead, "</tr></thead>")
+                 cols = values(lookup)
+                 features = [push!(val) for val in cols]
+                 for i in 1:count)
+                         obs = [row[i] for row in features]
+                         tbody = string(tbody, "<tr>")
+                         for observ in obs
+
+                         end
+                         [tbody = string(tbody, "<td>", observ,
+        "</td>") for observ in obs]
+                        tbody = string(tbody, "</tr>")
+                 end
+                 tbody = string(tbody, "</tbody>")
+                 final = string("<table>", thead, tbody, "</table")
+                 display("text/html", final)
+        end
+        function _drop(lookup::Dict, column::Symbol)
+                delete!(lookup, column)
+        end
+        function _drop(lookup::Dict, row::Int64)
+                [splice!(val[2], row) for val in lookup]
+        end
+        #==
+        THROWS
+        ==#
+        function length_check(lookup)
+                ourlen = length(ps[1])
+                [if length(x) != ourlen throw(DimensionMismatch(
+                "Columns must be the same size")) for x in ps]
+        end
 end
 
-end
-#=====
-Methods
-Many of these functional options will soon be replaced for their
-object-oriented counter-parts -- but not all!
-=====#
-function drop(od::OddFrame, symb::Symbol)
-    new_cop = Dict()
-    for (key, val) in od.lookup
-        if key != symb
-            push!(new_cop, key => val)
-        end
-    end
-    return(OddFrame(new_cop))
-end
-function drop(od::OddFrame, row::Int64)
-    vals = d.lookup
-    [splice!(val[2], row) for val in vals]
-    return(OddFrame(vals))
-end
+
 
 #===
 Indexing
