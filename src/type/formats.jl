3b3acc1caedf7c42976319fb27775a3f4f2d129f
Em <52672675+emmettgb@users.noreply.github.com>
2021-08-29 12:34:21 -0400

    added read_csv ability

---
+++ src/type/formats.jl
@@ -0,0 +1,14 @@
+function read_csv(csv_path::String)
+    readstr = open(f->read(f, String), csv_path, "r")
+    rows = split(readstr, '\n')
+    columns = split(rows[1], ',')
+    values = [[] for col in columns]
+    deleteat!(rows, 1)
+    for row in rows
+        data = (split(row, ','))
+        for col in 1:length(columns)
+            push!(values[col], data[col])
+        end
+    end
+    return(values, columns)
+end
