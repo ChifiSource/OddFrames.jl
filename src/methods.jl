7dea831c0d34c5eb2874427a9c24f5d55d148ce9
Em <52672675+emmettgb@users.noreply.github.com>
2021-08-31 10:27:05 -0400

    added size(), length(), width bindings

--- src/methods.jl
+++ src/methods.jl
@@ -1,2 +1,5 @@
 shape(od::AbstractOddFrame) = [length(od.labels), length(od.columns[1])]
-show(od::OddFrame) = od.head(5)
+size(od::AbstractOddFrame) = [length(od.labels), length(od.columns[1])]
+length(od::AbstractOddFrame) = length(od.columns[1])
+width(od::AbstractOddFrame) = length(od.labels)
+show(od::AbstractOddFrame) = od.head(length(od))
