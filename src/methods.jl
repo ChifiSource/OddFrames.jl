c2f8502242ab5d66e97c34979047cbab1e0d479d
Em <52672675+emmettgb@users.noreply.github.com>
2021-09-25 20:42:03 -0400

    added merge, removed select

--- src/methods.jl
+++ src/methods.jl
@@ -1,6 +1,18 @@
 import Base: show, size, length
+using Base.Docs: Binding
 shape(od::AbstractOddFrame) = [length(od.labels), length(od.columns[1])]
 size(od::AbstractOddFrame) = [length(od.labels), length(od.columns[1])]
 length(od::AbstractOddFrame) = length(od.columns[1])
 width(od::AbstractOddFrame) = length(od.labels)
 show(od::AbstractOddFrame) = od.head(length(od))
+function merge!(od::AbstractOddFrame, od2::AbstractOddFrame, at = width(od))
+
+end
+function _dtype(column, y)
+        try
+                [y(i) for i in column]
+        catch
+                throw(TypeError("column type casting",
+                y, column[1]))
+        end
+end
