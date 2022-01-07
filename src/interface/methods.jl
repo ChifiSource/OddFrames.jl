5ff6d52a349427c2fe3a8b2055717d9f996997c2
Em <52672675+emmettgb@users.noreply.github.com>
2022-01-07 14:00:09 -0500

    added method merge()

--- src/interface/methods.jl
+++ src/interface/methods.jl
@@ -6,22 +6,33 @@ length(od::AbstractOddFrame) = length(od.columns[1])
 width(od::AbstractOddFrame) = length(od.labels)
 show(od::AbstractOddFrame) = od.head(length(od))
 axis(od::AbstractOddFrame, col::Symbol) = findall(x->x==col, od.labels)[1]
-function merge!(od::AbstractMutableOddFrame,
-        od2::AbstractOddFrame; at::Int64 = width(od))
-        # Od2 column (for indexing) ,column number in at:width(od2)
-        # Columns identified by index position.
-        # This will mean the indexes are in order before they
-        #                            touch the OD.
-        if at < width(od)
-                after_push = [od.labels(at:width(od)), ]
-        end
-        for label in od2.labels
 
+function merge(od::AbstractOddFrame,
+        od2::AbstractOddFrame; at::Int64 = width(od) + 1)
+        if at > width(od) || at < 1
+                throw(BoundsError("Merge position is not an index on this OddFrame!"))
         end
+        pairs = []
+        for n in 1:width(od)
+                if n == at
+                        for n in 1:width(od2)
+                                push!(pairs, od2.labels[n] => od2.columns[n])
+                        end
+                end
+                push!(pairs, od.labels[n] => od.columns[n])
+        end
+        return(OddFrame(pairs))
 end
-# function merge!(od::AbstractMutableOddFrame,
-#        od2::AbstractOddFrame; at::Symbol = :A)
-#        at = axis(od, at)
-#
-
-#end
+function merge(od::AbstractOddFrame,
+        od2::AbstractOddFrame; at::Symbol = od.labels[width(od)])
+        pairs = []
+        for n in 1:width(od)
+                if od.labels[n] == at
+                        for n in 1:width(od2)
+                                push!(pairs, od2.labels[n] => od2.columns[n])
+                        end
+                end
+                push!(pairs, od.labels[n] => od.columns[n])
+        end
+        return(OddFrame(pairs))
+end
