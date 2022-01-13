d0c4ec672ebeeb997431f5505a4609998b12b2be
Em <52672675+emmettgb@users.noreply.github.com>
2022-01-12 23:22:44 -0500

    added vector mask indexing

--- src/interface/basetools.jl
+++ src/interface/basetools.jl
@@ -1,4 +1,4 @@
-import Base: push!
+import Base: push!, getindex
 
 function push!(iter::AbstractVector, val::Any; at::Int64 = length(iter))
     begin_vec = iter[1:at - 1]
@@ -9,4 +9,16 @@ function push!(iter::AbstractVector, val::Any; at::Int64 = length(iter))
     push!(iter, val)
     [push!(iter, v) for v in n_vec]
     iter
+end
+
+ikeys(pairs) = [p[1] for p in pairs]
+ivalues(pairs) = [p[2] for p in pairs]
+
+function getindex(x::AbstractArray, mask::BitArray)
+        pos = findall(x->x==0, mask)
+        [deleteat!(x, p) for p in pos]
+end
+function getindex(x::AbstractArray, mask::Function)
+    mask2 = [mask(x) for x in x]
+    getindex(x, mask2)
 end
