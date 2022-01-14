0f1f9bd1f05baef2def3ff48b7d94b97723522ff
Em <52672675+emmettgb@users.noreply.github.com>
2022-01-14 07:01:46 -0500

    documented iterable bindings

--- src/interface/index_iter.jl
+++ src/interface/index_iter.jl
@@ -1,11 +1,120 @@
 #===
 Iterators
 ===#
-frames(fg::FrameGroup) = [od for od in fg.ods]
+"""
+- **Interface**
+- Indexing and Iteration
+### frames(fg::OddFrameContainer)
+This quickly returns an iterable Array of the frames in a framegroup.
+- **posarg[1]** fg::AbstractOddFrameContainer => The fg should be the frame
+group you want to get the frames of.
+##### return
+- **[1]** ::Array{OddFrame} => Array of OddFrames that were contained in fg.
+##### example
+```
+labels = [:od1, :od2]
+fg = FrameGroup(OddFrame(:y => [5, 8, 2]), OddFrame(:x => [5, 15]),
+labels = labels)
+ods = frames(fg)
+```
+"""
+frames(fg::OddFrameContainer) = fg.ods
+"""
+- **Interface**
+- Indexing and Iteration
+### columns(od::AbstractOddFrame)
+This quickly returns an iterable Array of the columns in an OddFrame.
+- **posarg[1]** od::AbstractOddFrame => The OddFrame we wish to get the columns
+of.
+##### return
+- **[1]** ::Array{AbstractArray} => A 1-dimensional array of 1-dimensional
+features.
+##### example
+```
+od = OddFrame(:A => [1, 2, 3], :B => [5, 8, 3])
+columns(od)
+
+[[1, 2, 3], [5, 8, 3]]
+```
+"""
 columns(od::AbstractOddFrame) = od.columns
+"""
+- **Interface**
+- Indexing and Iteration
+### labels(od::AbstractOddFrame)
+This quickly returns an iterable Array of the labels in an OddFrame.
+- **posarg[1]** od::AbstractOddFrame => The OddFrame we wish to get the columns
+of.
+##### return
+- **[1]** ::Array{Symbol} => A 1-dimensional array of labels, Symbols.
+##### example
+```
+od = OddFrame(:A => [1, 2, 3], :B => [5, 8, 3])
+labels(od)
+
+[:A, :B]
+```
+"""
 labels(od::AbstractOddFrame) = od.labels
+"""
+- **Interface**
+- Indexing and Iteration
+### labels(fg::OddFrameContainer)
+This quickly returns an iterable Array of the frames in a framegroup.
+- **posarg[1]** fg::OddFrameContainer => The fg should be the frame
+group you want to get the labels of.
+##### return
+- **[1]** ::Array{Symbol} => Array of the labels inside the FrameGroup.
+##### example
+```
+labels = [:od1, :od2]
+fg = FrameGroup(OddFrame(:y => [5, 8, 2]), OddFrame(:x => [5, 15]),
+labels = labels)
+labels(fg)
+
+[:od1, :od2]
+```
+"""
 labels(fg::AbstractOddFrame) = fg.labels
-names(od::AbstractOddFrame) = [label for label in od.labels]
+"""
+- **Interface**
+- Indexing and Iteration
+### names(od::AbstractOddFrame)
+names() is just a binding for labels(). It returns an array of the labels
+found on the OddFrame.
+- **posarg[1]** od::AbstractOddFrame => The OddFrame we wish to get the columns
+of.
+##### return
+- **[1]** ::Array{Symbol} => A 1-dimensional array of labels, Symbols.
+##### example
+```
+od = OddFrame(:A => [1, 2, 3], :B => [5, 8, 3])
+names(od)
+
+[:A, :B]
+```
+"""
+names(od::AbstractOddFrame) = od.labels
+"""
+- **Interface**
+- Indexing and Iteration
+### names(fg::OddFrameContainer)
+names() is just a binding for labels(). It returns an array of the labels
+found on the OddFrame.
+- **posarg[1]** fg::OddFrameContainer => The fg should be the frame
+group you want to get the labels of.
+##### return
+- **[1]** ::Array{Symbol} => Array of the labels inside the FrameGroup.
+##### example
+```
+labels = [:od1, :od2]
+fg = FrameGroup(OddFrame(:y => [5, 8, 2]), OddFrame(:x => [5, 15]),
+labels = labels)
+names(fg)
+
+[:od1, :od2]
+```
+"""
 names(fg::OddFrameContainer) = [label for label in fg.labels]
 
 #==
