7952fc8bdc80a25f8a595a7ca206a1349d3ee7ec
Em <52672675+emmettgb@users.noreply.github.com>
2022-02-10 22:58:58 -0500

    added new describe (coldata -> describe)

--- src/type/member_func.jl
+++ src/type/member_func.jl
@@ -229,5 +229,25 @@ function describe(labels::Vector{Symbol}, columns::Vector{Any})
         pairs
 end
 function describe(col::Symbol, labels::Vector{Symbol}, columns::Vector{Any})
-
+        pairs = []
+        for (i, T) in enumerate(types)
+                if T == String
+                        push!(pairs, T => string("Data-type: ",
+                        T, "\nFeature Type: Categorical\n",
+                        "Categories: ", length(Set(columns[i]))))
+                elseif T == Bool
+                        push!(pairs, T => string("Data-type: ",
+                        T, "\nFeature Type: Categorical\n",
+                        "Categories: ", length(Set(columns[i]))))
+                elseif length(columns[i]) / length(Set(columns[i])) <= 1.8
+                        push!(pairs, T => string("Data-type: ",
+                        T, "\nFeature Type: Continuous\n", "Mean: ",
+                        mean(columns[i])))
+                else
+                        push!(pairs, T => string("Data-type: ",
+                        T, "\nFeature Type: Categorical\n",
+                        "Categories: ", length(Set(columns[i]))))
+                end
+        end
+        pairs
 end
